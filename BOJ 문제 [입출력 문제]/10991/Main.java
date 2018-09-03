1400b33c70d7f4f6b52b863daa74500196a293fc
lsw0833 <lsw0833@gmail.com>
2018-09-03 23:43:55 +0900

    [BOJ]10991

---
+++ BOJ 문제 [입출력 문제]/10991/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N-i;j++) {
+				System.out.print(" ");
+			} 
+			for(int j=1; j<=2*i-1; j++) {
+				if(j%2==0) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
