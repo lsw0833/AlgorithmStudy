7981f040f2fe3a7f94d0d7b68a772d7210b20164
lsw0833 <lsw0833@gmail.com>
2018-09-05 16:37:02 +0900

    [BOJ]11004

---
+++ BOJ 문제 [연습]/11004.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int [] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[K-1]);
+	}
+
+}
